90579a467376bd30e22769feb1399485ded8e401
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2020-07-11 12:44:51 +0530

    task manager using react hooks & context api

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import TaskListContextProvider from "./context/TaskListContext";
+import TaskForm from "./components/TaskForm";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <TaskListContextProvider>
+      <div className="container">
+        <div className="app-wrapper">
+        <Header />
+          <div className="main">
+            <TaskForm />
+            <TaskList />
+          </div>
+        </div>
+      </div>
+    </TaskListContextProvider>
+  );
+}
+
+export default App;
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { TaskListContext } from "../context/TaskListContext";
+import Task from "./Task";
+
+const TaskList = () => {
+  const { tasks } = useContext(TaskListContext);
+  return (
+    <div>
+      {tasks.length ? (
+        <ul className="list">
+          {tasks.map((task) => {
+            return <Task key={task.id} task={task} />;
+          })}
+        </ul>
+      ) : (
+        <div className="no-tasks">No Tasks</div>
+      )}
+    </div>
+  );
+};
+
+export default TaskList;
